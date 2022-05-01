a8bc08d87fad86f7e4eb33786e93957796e49d9e
ggongdori <soominlee0410@gmail.com>
2022-05-02 00:31:39 +0900

    modified

--- src/main/java/com/example/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/com/example/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package com.example.hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,5 +33,20 @@ public class ResponseHeaderServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
 //response.setContentLength(2); //(생략시 자동 생성)
+    }
+    private void cookie(HttpServletResponse response) {
+//Set-Cookie: myCookie=good; Max-Age=600;
+//response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600); //600초
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+//Status Code 302
+//Location: /basic/hello-form.html
+//response.setStatus(HttpServletResponse.SC_FOUND); //302
+//response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
     }
 }
